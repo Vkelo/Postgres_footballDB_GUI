9a9ecdabf936ef4aa6e9d74b227a965b495c3897
Kelo Anton Vili-Sakari <anton.vs.kelo@student.jyu.fi>
2023-05-09 15:01:37 +0300

    update95%

--- test.py
+++ test.py
@@ -40,12 +40,26 @@ class App(customtkinter.CTk):
                                                                command=self.change_scaling_event)
         self.scaling_optionemenu.grid(row=8, column=0, padx=20, pady=(10, 20))
 
-        # create main entry and button
+
+        #SEARCHVIEW
         self.entry = customtkinter.CTkEntry(self, placeholder_text="Search player by name")
         self.entry.grid(row=3, column=1, columnspan=2, padx=(20, 0), pady=(20, 20), sticky="nsew")
 
-        self.main_button_1 = customtkinter.CTkButton(master=self, fg_color="transparent", border_width=2, text_color=("gray10", "#DCE4EE"), text="hae")
+        def search_player():
+            cur = conn.cursor()
+            player_name = self.entry.get()
+            cur.execute("SELECT * FROM players WHERE name LIKE %s", (f"%{player_name}%",))
+            result = cur.fetchall()
+            self.textbox.delete("1.0", "end")
+            self.textbox.insert("1.0", result)
+            conn.commit()
+            cur.close()
+
+        self.main_button_1 = customtkinter.CTkButton(master=self, fg_color="transparent", border_width=2, text_color=("gray10", "#DCE4EE"), text="Search")
         self.main_button_1.grid(row=3, column=3, padx=(20, 20), pady=(20, 20), sticky="nsew")
+        self.main_button_1.configure(command=search_player)
+
+
 
         # create textbox
         self.textbox = customtkinter.CTkTextbox(self, width=400, height=450)
@@ -66,8 +80,13 @@ class App(customtkinter.CTk):
         self.button_12.grid(row=2, column=0, padx=20, pady=10)
 
         self.string_input_button = customtkinter.CTkButton(self.tabview.tab("DB SEARCH"), text="Search by keyword",
-                                                           command=self.open_input_dialog_event)
-        self.string_input_button.grid(row=3, column=0, padx=20, pady=(10, 10))
+                                                           command=self.input_dialog_event)
+        self.string_input_button.grid(row=3, column=0, padx=10, pady=(200, 10))
+
+        self.button15 = customtkinter.CTkButton(self.tabview.tab("DB SEARCH"), text="Search by team",
+                                                           command=self.search_team_event)
+        self.button15.grid(row=4, column=0, padx=10, pady=(10, 10))
+
 
         # create addview
         self.tabview = customtkinter.CTkTabview(self, width=200, height=400)
@@ -85,7 +104,7 @@ class App(customtkinter.CTk):
         self.string_input_button.grid(row=3, column=0, padx=20, pady=(10, 10))
 
 
-        self.string_input_button = customtkinter.CTkButton(self.tabview.tab("DB ADD"), text="ADD COMMENT",
+        self.string_input_button = customtkinter.CTkButton(self.tabview.tab("DB ADD"), text="ADD MATCH",
                                                            command=self.open_match_dialog)
         self.string_input_button.grid(row=4, column=0, padx=20, pady=(10, 10))
 
@@ -99,17 +118,21 @@ class App(customtkinter.CTk):
 
 
     # DIALOGS
-    def open_input_dialog_event(self):
+    def input_dialog_event(self):
         cur = conn.cursor()
         dialog = customtkinter.CTkInputDialog(text="Keyword:", title="SEARCH")
         keyword = dialog.get_input()
         if keyword is not None:
             cur.execute("""SELECT * FROM teams
-                        WHERE teams.name LIKE %s""",
-                        (f"%{keyword}%",))
+                        WHERE teams.city LIKE %s
+                        OR teams.country LIKE %s
+                        OR teams.name LIKE %s""",
+                        (f'%{keyword}%', f'%{keyword}%', f'%{keyword}%'))
             result = cur.fetchall()
             self.textbox.delete("1.0", "end")
-            self.textbox.insert("1.0", result)
+            for row in result:
+                row_str = ' '.join(str(val) for val in row)
+                self.textbox.insert("end", row_str + "\n")
         conn.commit()
         cur.close()
 
@@ -151,10 +174,34 @@ class App(customtkinter.CTk):
             conn.commit()
             cur.close()
 
-    def open_match_dialog(self):
-        dialog = customtkinter.CTkInputDialog(text="Your comment:", title="Add comment on the team")
-        print("CTkInputDialog:", dialog.get_input())
+    def search_team_event(self):
+        cur = conn.cursor()
+        dialog = customtkinter.CTkInputDialog(text="team name:", title="SEARCH")
+        cur.execute("SELECT MAX(team_id) FROM teams")
 
+        conn.commit()
+        cur.close()
+
+
+    def open_match_dialog(self):
+        cur = conn.cursor()
+        cur.execute("SELECT MAX(match_id) FROM matches")
+        max_id = cur.fetchone()[0]
+        if max_id is None:
+            max_id = 1000
+        match_id = max_id + 1
+        fields = ["Home team id", "Away team id", "League id", "Date", "Location"]
+        dialog = CustomDialog(self, fields, title="Add a match to DB")
+        self.wait_window(dialog)        
+        if dialog.result is not None:
+            home_team_id = dialog.result["Home team id"]
+            away_team_id = dialog.result["Away team id"]
+            league_id = dialog.result["League id"] 
+            date = dialog.result["Date"] 
+            location = dialog.result["Location"] 
+            cur.execute("INSERT INTO matches (match_id, home_team_id, away_team_id, league_id, date, location) VALUES (%s, %s, %s, %s, %s, %s)", (match_id, home_team_id, away_team_id, league_id, date, location))
+            conn.commit()
+            cur.close()
     def change_appearance_mode_event(self, new_appearance_mode: str):
         customtkinter.set_appearance_mode(new_appearance_mode)
 
@@ -346,3 +393,5 @@ class CustomDialog(tk.Toplevel):
 if __name__ == "__main__":
     app = App()
     app.mainloop()
+
+    #TODO: DEFAULT valinta liigahakuun, ADD match betting winnings???, Broaden keyword search and format, team selection with players
